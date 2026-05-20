1c001f916336453abc4e65c39a71dbbc8a569865
Kaick Ramos <202427640003@unifacol.edu.br>
2026-05-20 14:42:25 -0300

    Delete NasaApi directory

--- NasaApi/src/main/java/br/com/ramos/nasaapi/repository/NasaImageRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.ramos.nasaapi.repository;
-
-import br.com.ramos.nasaapi.model.NasaImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NasaImageRepository extends JpaRepository<NasaImage, Long> {
-}
