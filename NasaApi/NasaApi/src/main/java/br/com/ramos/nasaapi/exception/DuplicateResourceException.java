81aebe8a7eccc6782cd56110e584823b732ec254
Kaick Ramos <202427640003@unifacol.edu.br>
2026-05-21 13:07:25 -0300

    Delete NasaApi/NasaApi directory

--- NasaApi/NasaApi/src/main/java/br/com/ramos/nasaapi/exception/DuplicateResourceException.java
+++
@@ -1,7 +0,0 @@
-package br.com.ramos.nasaapi.exception;
-
-public class DuplicateResourceException extends RuntimeException {
-    public DuplicateResourceException(String message) {
-        super(message);
-    }
-}
--- NasaApi/NasaApi/src/main/java/br/com/ramos/nasaapi/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.com.ramos.nasaapi.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String resource, Long id) {
-        super(resource + " não encontrado com id: " + id);
-    }
-}
